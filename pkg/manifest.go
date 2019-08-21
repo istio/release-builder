1e45c7856c491aa4a23380c0aced0990ad2f85c8
John Howard <howardjohn@google.com>
2019-08-21 09:39:48 -0700

    Read from manifest file

--- pkg/manifest.go
+++ pkg/manifest.go
@@ -1,32 +1,22 @@
 package pkg
 
-import "github.com/howardjohn/istio-release/pkg/model"
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/ghodss/yaml"
+	"github.com/howardjohn/istio-release/pkg/model"
+)
 
 func ReadManifest(manifestFile string) (model.Manifest, error) {
-
-	return model.Manifest{
-		//Directory: "/tmp/istio-release365668469",
-		Version: "master-20190820-09-16",
-		Dependencies: []model.Dependency{
-			{
-				Org:    "istio",
-				Repo:   "istio",
-				Branch: "master",
-			},
-			{
-				Org:  "istio",
-				Repo: "cni",
-				Sha:  "master",
-			},
-		},
-		BuildOutputs: []model.BuildOutput{model.Helm},
-		//BuildOutputs: model.AllBuildOutputs,
-	}, nil
-	//by, err := ioutil.ReadFile(manifestFile)
-	//if err != nil {
-	//	log.Fatalf("failed to read manifest file: %v", err, )
-	//}
-	//manifest := pkg.Manifest{}
-	//err := yaml.Unmarshal(by, &manifest)
-	//return manifest, err
+	manifest := model.Manifest{}
+	by, err := ioutil.ReadFile(manifestFile)
+	if err != nil {
+		return manifest, fmt.Errorf("failed to read manifest file: %v", err)
+	}
+	if err := yaml.Unmarshal(by, &manifest); err != nil {
+		return manifest, fmt.Errorf("failed to unmarshal manifest file: %v", err)
+	}
+	manifest.BuildOutputs = []model.BuildOutput{model.Helm}
+	return manifest, nil
 }
