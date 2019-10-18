36d8802350b9c7782548907c65d9386b82609055
John Howard <howardjohn@google.com>
2019-10-18 12:32:55 -0700

    Use service account auth instead of docker (#25)

--- release/publish.sh
+++ release/publish.sh
@@ -19,13 +19,7 @@ WD=$(cd "$WD"; pwd)
 
 set -eux
 
-if [[ $(command -v gcloud) ]]; then
-  gcloud auth configure-docker -q
-elif [[ $(command -v docker-credential-gcr) ]]; then
-  docker-credential-gcr configure-docker
-else
-  echo "No credential helpers found, push to docker may not function properly"
-fi
+gcloud auth activate-service-account --key-file="$(GOOGLE_APPLICATION_CREDENTIALS)"
 
 VERSION="$(cat "${WD}/trigger-publish")"
 
