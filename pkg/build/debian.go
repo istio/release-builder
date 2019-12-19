56f21736b7956ef451a98516f8f6919b4b3b6a49
Steven Dake <sdake@ibm.com>
2019-12-19 09:01:44 -0700

    Expect debian binaries in the TARGET_OUT directory (#105)
    * Expect debian binaries in the TARGET_OUT directory
    
    * Address reviewer comments

--- pkg/build/debian.go
+++ pkg/build/debian.go
@@ -27,7 +27,7 @@ func Debian(manifest model.Manifest) error {
 	if err := util.RunMake(manifest, "istio", nil, "sidecar.deb"); err != nil {
 		return fmt.Errorf("failed to build sidecar.deb: %v", err)
 	}
-	if err := util.CopyFile(path.Join(manifest.GoOutDir(), "istio-sidecar.deb"), path.Join(manifest.OutDir(), "deb", "istio-sidecar.deb")); err != nil {
+	if err := util.CopyFile(path.Join(manifest.RepoOutDir("istio"), "istio-sidecar.deb"), path.Join(manifest.OutDir(), "deb", "istio-sidecar.deb")); err != nil {
 		return fmt.Errorf("failed to package istio-sidecar.deb: %v", err)
 	}
 	if err := util.CreateSha(path.Join(manifest.OutDir(), "deb", "istio-sidecar.deb")); err != nil {
