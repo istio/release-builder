fe6265b53b27a4b9cbad1d20d36b3c980ed1d122
Steven Dake <sdake@ibm.com>
2019-12-19 08:13:49 -0700

    Address reviewer comments

--- pkg/build/debian.go
+++ pkg/build/debian.go
@@ -27,10 +27,8 @@ func Debian(manifest model.Manifest) error {
 	if err := util.RunMake(manifest, "istio", nil, "sidecar.deb"); err != nil {
 		return fmt.Errorf("failed to build sidecar.deb: %v", err)
 	}
-	if util.FileExists(path.Join(manifest.RepoOutDir("istio"), "deb")) {
-		if err := util.CopyFile(path.Join(manifest.RepoOutDir("istio"), "istio-sidecar.deb"), path.Join(manifest.OutDir(), "deb", "istio-sidecar.deb")); err != nil {
-			return fmt.Errorf("failed to package istio-sidecar.deb: %v", err)
-		}
+	if err := util.CopyFile(path.Join(manifest.RepoOutDir("istio"), "istio-sidecar.deb"), path.Join(manifest.OutDir(), "deb", "istio-sidecar.deb")); err != nil {
+		return fmt.Errorf("failed to package istio-sidecar.deb: %v", err)
 	}
 	if err := util.CreateSha(path.Join(manifest.OutDir(), "deb", "istio-sidecar.deb")); err != nil {
 		return fmt.Errorf("failed to package istio-sidecar.deb: %v", err)
