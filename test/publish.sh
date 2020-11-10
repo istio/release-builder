4d4d58c5e0360fc6f1c7eaedbb13a245abb748e7
Eric Van Norman <ericvn@us.ibm.com>
2020-11-10 13:53:27 -0600

    Update to point to envoyproxy/envoyproxy (#474)

--- test/publish.sh
+++ test/publish.sh
@@ -66,7 +66,7 @@ dependencies:
     git: https://github.com/istio/tools
     branch: master
   envoy:
-    git: https://github.com/istio/envoy
+    git: https://github.com/envoyproxy/envoy
     auto: proxy_workspace
 dashboards:
   istio-extension-dashboard: 13277
