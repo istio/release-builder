7e64c7694deab2eabff4bb4e64531160855198c1
shamsher31 <shaansar@redhat.com>
2021-01-14 22:59:15 +0530

    keep master for test-infra

--- release/build.sh
+++ release/build.sh
@@ -69,7 +69,7 @@ ${DEPENDENCIES:-$(cat <<EOD
     branch: release-1.9
   test-infra:
     git: https://github.com/istio/test-infra
-    branch: release-1.9
+    branch: master
   tools:
     git: https://github.com/istio/tools
     branch: release-1.9
