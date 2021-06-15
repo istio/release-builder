bd36f5a11365c6955a6e605cf6e707a1c3df3451
Eric Van Norman <ericvn@us.ibm.com>
2021-06-15 13:12:32 -0500

    replace github commands with go-git

--- go.mod
+++ go.mod
@@ -5,10 +5,13 @@ go 1.16
 require (
 	cloud.google.com/go/storage v1.15.0
 	github.com/Masterminds/semver/v3 v3.1.1
+	github.com/go-git/go-git v4.7.0+incompatible
+	github.com/go-git/go-git/v5 v5.4.2
 	github.com/google/go-github/v35 v35.2.0
 	github.com/rogpeppe/go-internal v1.8.0
 	github.com/spf13/cobra v1.1.3
 	golang.org/x/oauth2 v0.0.0-20210427180440-81ed05c6b58c
+	gopkg.in/src-d/go-git.v4 v4.13.1 // indirect
 	istio.io/pkg v0.0.0-20210504140408-703cba576e59
 	sigs.k8s.io/yaml v1.2.0
 )
--- pkg/util/git.go
+++ pkg/util/git.go
@@ -15,14 +15,17 @@
 package util
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"io/ioutil"
 	"os"
-	"os/exec"
 	"strings"
+	"time"
 
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/google/go-github/v35/github"
 	"golang.org/x/oauth2"
 
@@ -30,92 +33,135 @@ import (
 	"istio.io/release-builder/pkg/model"
 )
 
-// PushCommit will create a branch and push a commit with the specified commit text
-func PushCommit(manifest model.Manifest, repo, branch, commitString string, dryrun bool, githubToken string) (changes bool, err error) {
-	output := bytes.Buffer{}
-	cmd := VerboseCommand("git", "status", "--porcelain")
-	cmd.Dir = manifest.RepoDir(repo)
-	cmd.Stdout = &output
-	if err := cmd.Run(); err != nil {
-		return false, err
+// PushCommit will look for changes. If changes exist, it will create a branch and push a commit with the specified commit text
+// to the upstremam repo.
+func PushCommit(manifest model.Manifest, repo, branch, commitString string, dryrun bool, githubToken string, user github.User) (changes bool, err error) {
+	// Use go-git since it will take an already cloned and changed file-system and use that as a
+	// working tree to create the commit instead of using `git` commands. This allows the use of
+	// the passed in github token without it leaking in the logs.
+	r, err := git.PlainOpen(manifest.RepoDir(repo))
+	if err != nil {
+		return false, fmt.Errorf("failed to open path: %v", err)
 	}
-	if output.Len() == 0 {
+	w, err := r.Worktree()
+	if err != nil {
+		return false, fmt.Errorf("failed to retrieve work tree: %v", err)
+	}
+
+	// Git the worktree status to see if there are any changes. Return if none.
+	status, err := w.Status()
+	if err != nil {
+		return false, fmt.Errorf("failed to retrieve status: %v", err)
+	}
+	if status.IsClean() {
 		log.Infof("no changes found to commit")
 		return false, nil
 	}
-	log.Infof("changes found:\n%s", &output)
+	log.Infof("changes found:\n%v", &status)
 
+	// If a dry_run, create a commit and push to the upstream repo
 	if !dryrun {
-		cmd = VerboseCommand("git", "checkout", "-b", branch)
-		cmd.Dir = manifest.RepoDir(repo)
-		if err := cmd.Run(); err != nil {
-			return true, err
+		// Add the changed files to staging
+		// w.AddWithOptions added some ignored files, like out/.env, so possibly
+		// an issue with the library. Add the files noted as changed in the worktree status.
+		for changedFile := range status {
+			_, err = w.Add(changedFile)
+			if err != nil {
+				return true, fmt.Errorf("failed to add file to staging %s: %v", changedFile, err)
+			}
 		}
 
-		cmd = VerboseCommand("git", "add", "-A")
-		cmd.Dir = manifest.RepoDir(repo)
-		if err := cmd.Run(); err != nil {
-			return true, err
-		}
-
-		// Get user.email and user.name from the GitHub token
-		ctx := context.Background()
-		ts := oauth2.StaticTokenSource(
-			&oauth2.Token{AccessToken: githubToken},
-		)
-
-		tc := oauth2.NewClient(ctx, ts)
-		client := github.NewClient(tc)
-
-		user, _, err := client.Users.Get(ctx, "")
+		// Checkout the specified branch
+		err = w.Checkout(&git.CheckoutOptions{
+			Branch: plumbing.NewBranchReferenceName(branch),
+			Create: true,
+			Keep:   true,
+		})
 		if err != nil {
-			return true, err
+			return true, fmt.Errorf("failed to checkout branch: %v", err)
 		}
 
+		// Create a commit on that branch
 		// user.Email may be nil, so set to an empty string
 		emptyString := ""
 		if user.Email == nil {
 			user.Email = &emptyString
 		}
-
-		cmd = VerboseCommand("git", "-c", "user.name="+*user.Name, "-c", "user.email="+*user.Email, "commit",
-			"--message", commitString, "--author="+*user.Name+"<"+*user.Email+">")
-		cmd.Dir = manifest.RepoDir(repo)
-		if err := cmd.Run(); err != nil {
-			return true, err
+		commit, err := w.Commit(commitString, &git.CommitOptions{
+			Author: &object.Signature{
+				Name:  *user.Name,
+				Email: *user.Email,
+				When:  time.Now(),
+			},
+		})
+		if err != nil {
+			return true, fmt.Errorf("failed to create commit: %v", err)
 		}
+		log.Infof("commit created:\n%v", commit)
 
-		cmd = VerboseCommand("git", "push", "--set-upstream", "origin", branch)
-		cmd.Dir = manifest.RepoDir(repo)
-		if err := cmd.Run(); err != nil {
-			return true, err
+		// Push to the upstream repo.
+		err = r.Push(&git.PushOptions{
+			Auth: &http.BasicAuth{
+				Username: *user.Name, // yes, this can be anything except an empty string
+				Password: githubToken,
+			},
+		})
+		if err != nil {
+			return true, fmt.Errorf("failed to push: %v", err)
 		}
 	}
 	return true, nil
 }
 
-// CreatePR will look for changes. If changes exist, it will create
-// a branch and push a commit with the specified commit text
+// CreatePR will look for changes. If changes exist, it will create a branch and push a commit with
+// the specified commit text, and then create a PR in the upstream repo.
 func CreatePR(manifest model.Manifest, repo, branch, commitString string, dryrun bool, githubToken string) error {
-	changes, err := PushCommit(manifest, repo, branch, commitString, dryrun, githubToken)
+	// Get user from the GitHub token
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: githubToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	user, _, err := client.Users.Get(ctx, "")
+	if err != nil {
+		return err
+	}
+
+	changes, err := PushCommit(manifest, repo, branch, commitString, dryrun, githubToken, *user)
 	if err != nil {
 		return err
 	}
 
 	if changes && !dryrun {
-		var cmd *exec.Cmd
-		// Don't label the envoy or non istio repos with `release-notes-none`
-		if repo == "envoy" || !strings.Contains(manifest.Dependencies.Get()[repo].Git, "github.com/istio/") {
-			cmd = VerboseCommand("gh", "pr", "create", "--repo", manifest.Dependencies.Get()[repo].Git,
-				"--fill", "--head", branch, "--base", manifest.Dependencies.Get()[repo].Branch)
-		} else {
-			cmd = VerboseCommand("gh", "pr", "create", "--repo", manifest.Dependencies.Get()[repo].Git,
-				"--fill", "--head", branch, "--base", manifest.Dependencies.Get()[repo].Branch, "--label", "release-notes-none")
+		newPR := &github.NewPullRequest{
+			Title:               &commitString,
+			Head:                &branch,
+			Base:                &manifest.Dependencies.Get()[repo].Branch,
+			MaintainerCanModify: github.Bool(true),
 		}
-		cmd.Dir = manifest.RepoDir(repo)
-		if err := cmd.Run(); err != nil {
+
+		repoStrings := strings.Split(manifest.Dependencies.Get()[repo].Git, "/")
+		l := len(repoStrings)
+		orgString := repoStrings[l-2]
+		repoString := repoStrings[l-1]
+
+		pr, _, err := client.PullRequests.Create(ctx, orgString, repoString, newPR)
+		if err != nil {
 			return err
 		}
+
+		log.Infof("PR created: %s\n", pr.GetHTMLURL())
+
+		// Add a release-notes-none label on the PR if istio org, but not envoy repo.
+		if orgString == "istio" && repoString != "envoy" {
+			label, _, err := client.Issues.AddLabelsToIssue(ctx, orgString, repoString, *pr.Number, []string{"release-notes-none"})
+			if err != nil {
+				return err
+			}
+			log.Infof("Labels:\n%v", label)
+		}
+
 	}
 	return nil
 }
