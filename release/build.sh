76f2c1010b2aeac152f91c5d0880398ee32a3ed1
Daniel Grimm <dgrimm@redhat.com>
2021-10-28 21:35:55 +0200

    [release-1.12] Automated branching step 4 (#823)

--- release/build.sh
+++ release/build.sh
@@ -61,7 +61,7 @@ dependencies:
 ${DEPENDENCIES:-$(cat <<EOD
   istio:
     git: https://github.com/istio/istio
-    branch: master
+    branch: release-1.12
   api:
     git: https://github.com/istio/api
     auto: modules
@@ -73,17 +73,17 @@ ${DEPENDENCIES:-$(cat <<EOD
     auto: modules
   client-go:
     git: https://github.com/istio/client-go
-    branch: master
+    branch: release-1.12
     goversionenabled: true
   gogo-genproto:
     git: https://github.com/istio/gogo-genproto
-    branch: master
+    branch: release-1.12
   test-infra:
     git: https://github.com/istio/test-infra
     branch: master
   tools:
     git: https://github.com/istio/tools
-    branch: master
+    branch: release-1.12
   envoy:
     git: https://github.com/envoyproxy/envoy
     auto: proxy_workspace
@@ -111,7 +111,7 @@ directory: "${WORK_DIR}"
 dependencies:
   istio:
     git: https://github.com/istio/istio
-    branch: master
+    branch: release-1.12
 EOF
 )
   go run main.go build \
