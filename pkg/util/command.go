853d20126bf3b815f0dd0157efbe1ffb28c35cf1
John Howard <howardjohn@google.com>
2022-08-03 16:02:19 -0700

    Support publishing multi-arch images

--- pkg/util/command.go
+++ pkg/util/command.go
@@ -35,6 +35,7 @@ func StandardEnv(manifest model.Manifest) []string {
 		"BUILD_WITH_CONTAINER=0", // Build should already run in container, having multiple layers of docker causes issues
 		"IGNORE_DIRTY_TREE=1",
 		"INCLUDE_UNTAGGED_DEFAULT=true",
+		"DOCKER_ARCHITECTURES="+strings.Join(manifest.Architectures, ","),
 	)
 	return env
 }
