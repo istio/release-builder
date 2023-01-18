91220326dc177abcb64e495b6b7f3970678eb6c1
Andrea Ma <ayma@us.ibm.com>
2023-01-18 15:03:48 -0600

    [release-1.17] Automated branching step 4

--- test/publish.sh
+++ test/publish.sh
@@ -50,7 +50,7 @@ dependencies:
 ${DEPENDENCIES:-$(cat <<EOD
   istio:
     git: https://github.com/${GITHUB_ORG}/istio
-    branch: master
+    branch: release-1.17
   api:
     git: https://github.com/${GITHUB_ORG}/api
     auto: modules
@@ -62,20 +62,20 @@ ${DEPENDENCIES:-$(cat <<EOD
     auto: modules
   client-go:
     git: https://github.com/${GITHUB_ORG}/client-go
-    branch: master
+    branch: release-1.17
     goversionenabled: true
   test-infra:
     git: https://github.com/${GITHUB_ORG}/test-infra
     branch: master
   tools:
     git: https://github.com/${GITHUB_ORG}/tools
-    branch: master
+    branch: release-1.17
   envoy:
     git: https://github.com/envoyproxy/envoy
     auto: proxy_workspace
   release-builder:
     git: https://github.com/${GITHUB_ORG}/release-builder
-    branch: master
+    branch: release-1.17
 EOD
 )}
 dashboards:
