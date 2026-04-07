fb5a4c1f78b81797225aee91b5064c9284ef93ff
Steven Jin Xuan <sjinxuan@microsoft.com>
2026-04-07 16:59:29 -0400

    retires

--- pkg/publish/r2.go
+++ pkg/publish/r2.go
@@ -77,31 +77,53 @@ func ArchiveR2(manifest model.Manifest, bucket string, aliases []string) error {
 	return nil
 }
 
+// MutateObjectR2 pulls a file from R2, mutates it, then pushes it back up.
+// It uses ETag-based conditional writes to retry if the object was modified concurrently.
 func MutateObjectR2(outDir string, client *s3.Client, bkt *string, objectPrefix string, filename string, f func() error) error {
+	for i := 0; i < 10; i++ {
+		err := mutateObjectR2Inner(outDir, client, bkt, objectPrefix, filename, f)
+		if err == ErrIndexOutOfDate {
+			log.Warnf("Write conflict, trying again")
+			continue
+		}
+		return err
+	}
+	return fmt.Errorf("max conflicts attempted")
+}
+
+func mutateObjectR2Inner(outDir string, client *s3.Client, bkt *string, objectPrefix string, filename string, f func() error) error {
 	objName := filepath.Join(objectPrefix, filename)
 	outFile := filepath.Join(outDir, filename)
 	ctx := context.Background()
-	{
-		obj, err := client.GetObject(ctx, &s3.GetObjectInput{
-			Bucket: bkt,
-			Key:    ptr.String(objName),
-		})
-		if err != nil {
-			if !strings.Contains(err.Error(), "NoSuchKey") {
-				return fmt.Errorf("failed to fetch object %s: %v", objName, err)
-			}
-		} else {
-			defer obj.Body.Close()
-			idx, err := os.Create(outFile)
-			if err != nil {
-				return fmt.Errorf("failed to create %v: %v", outFile, err)
-			}
-			defer idx.Close()
-			if _, err := idx.ReadFrom(obj.Body); err != nil {
-				return fmt.Errorf("failed to read object body: %v", err)
-			}
-			log.Infof("Wrote %v", outFile)
+
+	// Track the ETag for conditional writes
+	var etag *string
+
+	obj, err := client.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: bkt,
+		Key:    ptr.String(objName),
+	})
+	if err != nil {
+		if !strings.Contains(err.Error(), "NoSuchKey") {
+			return fmt.Errorf("failed to fetch object %s: %v", objName, err)
 		}
+		log.Warnf("existing file %v does not exist", filename)
+	} else {
+		etag = obj.ETag
+		log.Infof("Object %v currently has ETag %s", objName, *etag)
+
+		idx, err := os.Create(outFile)
+		if err != nil {
+			return fmt.Errorf("failed to create %v: %v", outFile, err)
+		}
+		if _, err := idx.ReadFrom(obj.Body); err != nil {
+			idx.Close()
+			obj.Body.Close()
+			return fmt.Errorf("failed to read object body: %v", err)
+		}
+		idx.Close()
+		obj.Body.Close()
+		log.Infof("Wrote %v", outFile)
 	}
 
 	// Run our action
@@ -109,18 +131,31 @@ func MutateObjectR2(outDir string, client *s3.Client, bkt *string, objectPrefix 
 		return fmt.Errorf("action failed: %v", err)
 	}
 
-	// Now we want to (try to) write it
+	// Now write it back with a conditional put
 	idx, err := os.Open(outFile)
 	if err != nil {
 		return fmt.Errorf("failed to open %v: %v", outFile, err)
 	}
 	defer idx.Close()
-	_, err = client.PutObject(ctx, &s3.PutObjectInput{
-		Bucket: bkt,
-		Key:    ptr.String(objName),
-		Body:   idx,
-	})
+
+	putInput := &s3.PutObjectInput{
+		Bucket:       bkt,
+		Key:          ptr.String(objName),
+		Body:         idx,
+		ContentType:  ptr.String("text/yaml"),
+		CacheControl: ptr.String("no-cache, max-age=0, no-transform"),
+	}
+	// If the object existed, use If-Match to ensure it hasn't been modified since we read it.
+	if etag != nil {
+		putInput.IfMatch = etag
+	}
+
+	_, err = client.PutObject(ctx, putInput)
 	if err != nil {
+		// A 412 Precondition Failed means someone else wrote to the object since we read it.
+		if strings.Contains(err.Error(), "PreconditionFailed") || strings.Contains(err.Error(), "412") {
+			return ErrIndexOutOfDate
+		}
 		return fmt.Errorf("failed to write object %s: %v", objName, err)
 	}
 	return nil
