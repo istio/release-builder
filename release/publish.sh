5df4ded567803c07b0bf82ab544fb717e1089259
Steven Jin Xuan <sjinxuan@microsoft.com>
2026-08-27 13:34:34 -0400

    Partial cherry pick/adaptation of #2325, #2398, #2406

--- release/publish.sh
+++ release/publish.sh
@@ -40,9 +40,14 @@ VERSION="$(cat "${WD}/trigger-publish")"
 SOURCE_GCS_BUCKET=${SOURCE_GCS_BUCKET:-istio-prerelease/prerelease}
 GCS_BUCKET=${GCS_BUCKET:-istio-release/releases}
 HELM_BUCKET=${HELM_BUCKET:-istio-release/charts}
-HELM_HUB_RELEASE=${HELM_HUB_RELEASE:-gcr.io/istio-release/charts}
+R2_BUCKET=${R2_BUCKET:-istio-release/releases}
+R2_HELM_BUCKET=${R2_HELM_BUCKET:-istio-release/charts}
+R2_HELM_URL=${R2_HELM_URL:-https://blob.istio.io/istio-release/charts}
+# We actually push to these hubs. This doesn't affect the default hub in Helm charts
 DOCKER_HUB=${DOCKER_HUB:-docker.io/istio}
 DOCKER_HUB_MIRROR=${DOCKER_HUB_MIRROR:-gcr.io/istio-release}
+HELM_HUB_RELEASE=${HELM_HUB_RELEASE:-ghcr.io/istio/release/charts}
+HELM_HUB_RELEASE_MIRROR=${HELM_HUB_RELEASE_MIRROR:-ghcr.io/istio/release/charts}
 GITHUB_ORG=${GITHUB_ORG:-istio}
 GITHUB_TOKEN_FILE=${GITHUB_TOKEN_FILE:-}
 GRAFANA_TOKEN_FILE=${GRAFANA_TOKEN_FILE:-}
@@ -52,13 +57,24 @@ WORK_DIR="$(mktemp -d)/release"
 mkdir -p "${WORK_DIR}"
 
 gsutil -m cp -r "gs://${SOURCE_GCS_BUCKET}/${VERSION}/*" "${WORK_DIR}"
-go run main.go publish --release "${WORK_DIR}" \
+
+ENDPOINT="$(echo "${CF_CREDENTIALS}" | jq -r '.endpoint' | tr -d '\n')"
+AWS_ACCESS_KEY_ID="$(echo "${CF_CREDENTIALS}" | jq -r '.access_key' | tr -d '\n')" \
+    AWS_SECRET_ACCESS_KEY="$(echo "${CF_CREDENTIALS}" | jq -r '.secret_key' | tr -d '\n')" \
+    AWS_REGION="$(echo "${CF_CREDENTIALS}" | jq -r '.region' | tr -d '\n')" \
+    AWS_SESSION_TOKEN="$(echo "${CF_CREDENTIALS}" | jq -r '.session_token' | tr -d '\n')" \
+    go run main.go publish --release "${WORK_DIR}" \
     --cosignkey "${COSIGN_KEY:-}" \
     --gcsbucket "${GCS_BUCKET}" \
     --helmbucket "${HELM_BUCKET}" \
+    --helmhub "${HELM_HUB_RELEASE_MIRROR}" \
+    --s3bucket "${R2_BUCKET}" \
+    --s3helmbucket "${R2_HELM_BUCKET}" \
+    --s3helmurl "${R2_HELM_URL}" \
     --dockerhub "${DOCKER_HUB}" --dockertags "${VERSION}" \
     --github "${GITHUB_ORG}" --githubtoken "${GITHUB_TOKEN_FILE}" \
-    --grafanatoken "${GRAFANA_TOKEN_FILE}"
+    --grafanatoken "${GRAFANA_TOKEN_FILE}" \
+    --s3-base-endpoint "${ENDPOINT}"
 
 # Also push images to a GCR repo, in case of dockerhub rate limiting issues for
 # large clusters (see https://docs.docker.com/docker-hub/download-rate-limit/).
