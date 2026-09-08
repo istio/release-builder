c75b8aa0461a6a5b2c04fed3a3f318b752aa8b6a
Jacek Ewertowski <jacek.ewertowski1@gmail.com>
2026-09-08 11:12:40 +0200

    Don't use gcr.io for fake-gcs-server
    Signed-off-by: Jacek Ewertowski <jacek.ewertowski1@gmail.com>

--- test/publish.sh
+++ test/publish.sh
@@ -41,7 +41,7 @@ docker run -d  --rm  \
 docker run -d  --rm  \
   -p "7481:7481" --label istio-release-builder \
   --name "release-builder-gcs" \
-  gcr.io/istio-testing/fake-gcs-server:1.52.3 \
+  registry.istio.io/testing/fake-gcs-server:1.52.3 \
   -scheme http -port 7481
 
 # Setup our bucket. Add retry since the registry may not be ready yet
