bc47635b78df878a5d1a817e01540cfd8ccd03ea
Jacek Ewertowski <jacek.ewertowski1@gmail.com>
2026-09-11 00:41:34 +0200

    [release-1.31] update registry image and update common-files (#2429)
    * Automator: update common-files@release-1.31 in istio/release-builder@release-1.31
    
    * Update registry
    
    Signed-off-by: Jacek Ewertowski <jacek.ewertowski1@gmail.com>
    
    * Don't use gcr.io for fake-gcs-server
    
    Signed-off-by: Jacek Ewertowski <jacek.ewertowski1@gmail.com>
    
    * Don't require credentials when GCS_HOST is localhost
    
    Signed-off-by: Jacek Ewertowski <jacek.ewertowski1@gmail.com>
    
    ---------
    
    Signed-off-by: Jacek Ewertowski <jacek.ewertowski1@gmail.com>
    Co-authored-by: istio-testing <istio-testing-bot@google.com>

--- test/publish.sh
+++ test/publish.sh
@@ -37,11 +37,11 @@ trap cleanup EXIT
 docker run -d  --rm  \
   -p "7480:5000" --label istio-release-builder \
   --name "release-builder-registry" \
-  gcr.io/istio-testing/registry:2
+  registry.istio.io/testing/registry:2
 docker run -d  --rm  \
   -p "7481:7481" --label istio-release-builder \
   --name "release-builder-gcs" \
-  gcr.io/istio-testing/fake-gcs-server:1.52.3 \
+  registry.istio.io/testing/fake-gcs-server:1.52.3 \
   -scheme http -port 7481
 
 # Setup our bucket. Add retry since the registry may not be ready yet
