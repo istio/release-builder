0f094fa9b7d73b4ed9cd7e4b793ee8f5ecf1b0c8
Steven Jin Xuan <sjinxuan@microsoft.com>
2026-09-18 12:32:42 -0400

    fix test script

--- test/publish.sh
+++ test/publish.sh
@@ -33,10 +33,6 @@ function cleanup() {
 }
 trap cleanup EXIT
 
-# MinIO receives virtual-hosted S3 requests, so map the test bucket hostname
-# to the local port-forward from the build-tools container.
-echo "127.0.0.1 istio-build.localhost" >> /etc/hosts
-
 # Setup local registry and S3-compatible storage.
 docker run -d  --rm  \
   -p "7480:5000" --label istio-release-builder \
@@ -134,6 +130,11 @@ go run main.go build --manifest <(echo "${MANIFEST}")
 go run main.go validate --release "${WORK_DIR}/out"
 
 if [[ -z "${DRY_RUN:-}" ]]; then
+# MinIO receives virtual-hosted S3 requests, so map the test bucket hostname
+# to the local port-forward after Prow finishes initializing the pod hosts file.
+echo "127.0.0.1 istio-build.localhost" >> /etc/hosts
+getent hosts istio-build.localhost
+
 go run main.go publish --release "${WORK_DIR}/out" \
   --cosignkey "${COSIGN_KEY:-}" \
   --helmbucket "${HELM_BUCKET}" \
